706f3e6e673aa3117384c6430dffe7d8f9bb5a78
atibayan <aemilia.tibayan@gmail.com>
2023-03-13 10:43:08 -0700

    update view/delete products

--- src/components/ProductUploader.js
+++ src/components/ProductUploader.js
@@ -3,8 +3,34 @@ import { faPlus, faTrashCan, faX} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios'
 import './ProductUploader.css'
+import Resizer from 'react-image-file-resizer'
 const serverUrl = process.env.REACT_APP_SERVER_URL
 
+const Product = (props) => {
+    return (
+      <div style={{"border": "1px solid black"}}>
+        <p>{props.details.name}</p>
+        <p>{props.details.price}</p>
+        <p>{props.details.description}</p>
+        {props.details.tags.map(tag => {
+          return (
+            <p key={tag}>TAG: {tag}</p>
+          )
+        })}
+        {
+          props.details.images.map(img => {
+            return (
+              <div key={img.image_name}>
+              <img src={img.signedImage}></img> 
+              <p>{img.image_name}</p>
+              </div>
+            )
+          })
+        }
+        <button onClick={() => props.handleDeleteProduct(props.id)}>Delete</button>
+      </div>
+    )
+}
 
 const ImageUploader = () => {
   const [selectedImages, setSelectedImages] = useState([]);
@@ -13,6 +39,8 @@ const ImageUploader = () => {
   const [description, setDescription] = useState("");
   const [tags, setTags] = useState([]);
   const [qtyOnHand, setQtyOnHand] = useState(0)
+
+  const [products, setProducts] = useState([])
 
   const onSelectFile = e => {
     const selectedFilesArray = Array.from(e.target.files);
@@ -30,25 +58,45 @@ const ImageUploader = () => {
     URL.revokeObjectURL(imageUrl);
   }
 
+  const resizeFile = (file) => new Promise(resolve => {
+    Resizer.imageFileResizer(file, 300, 300, 'JPEG', 100, 0,
+      uri => {
+        resolve(uri);
+      }, 'blob' );
+  });
+
   const submit = async e => {
     e.preventDefault();
+    const product_info = {
+      name,
+      price,
+      description,
+      qtyOnHand
+    }
+    const prod_info = await axios.post(`${serverUrl}product`, product_info)
+    console.log(prod_info.data)
+
     const formData = new FormData();
     for(let i = 0; i < selectedImages.length; i++){
-      let blob = await fetch(selectedImages[i].url).then(r => r.blob())
-      formData.append("image", blob)
+      const blob = await fetch(selectedImages[i].url).then(r => r.blob())
+      const resized_blob = await resizeFile(blob)
+      formData.append("image", resized_blob)
     }
-    formData.append("name", name)
-    formData.append("price", price)
-    formData.append("description", description)
-    formData.append("qtyOnHand", qtyOnHand)
 
-    for(let i = 0; i < tags.length; i++){
-      formData.append("tags", tags[i])
-    }
-    tags.map(tag => {
-      formData.append("tags", tag)
+    const prod_photo = await axios.post(
+      `${serverUrl}product/${prod_info.data.pid}/photos`,
+      formData,
+      { headers: {'Content-Type': 'multipart/form-data'}
     })
-    await axios.post(`${serverUrl}product`, formData, { headers: {'Content-Type': 'multipart/form-data'}})
+
+    const prod_tag = await axios.post(
+      `${serverUrl}product/${prod_info.data.pid}/tags`,
+      {tags})
+  }
+
+  const getProducts = async () => {
+    const {data} = await axios.get(`${serverUrl}product`)
+    setProducts(prevProducts => [...prevProducts, ...data.products])
   }
 
   const addTag = e => {
@@ -65,6 +113,12 @@ const ImageUploader = () => {
   const deleteTag = deletedTag => {
     setTags(prevTags => prevTags.filter(tag => tag !== deletedTag))
   }
+
+  const handleDeleteProduct = async (id) => {
+      const res = await axios.delete(`${serverUrl}product/${id}`)
+      if (res.status == 200)
+        setProducts(prevProducts => prevProducts.filter(p => p._id != id))
+    }
 
   return (
     <div>
@@ -101,8 +155,7 @@ const ImageUploader = () => {
             name="images"
             onChange={onSelectFile}
             multiple
-            accept="image/*"
-            required
+            accept="image/jpeg, image/png, image/gif"
           />
           
           <div className="images">
@@ -121,6 +174,14 @@ const ImageUploader = () => {
           <button className="upload-btn" type="submit">Submit</button>
         </div>
       </form>
+      <button className="upload-btn" onClick={getProducts}>Get Products</button>
+      <div className='product-viewer'>
+        {
+          products.map(p => {
+            return <Product key={p._id} id={p._id} details={p} handleDeleteProduct={handleDeleteProduct} />
+          })
+        }
+      </div>
     </div>
   );
 };
