6f055315baa3b7eeef2ee3c683d997e0f96e9006
kayleephan <122582001+kayleephan@users.noreply.github.com>
2023-03-13 17:18:31 -0700

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,110 @@
+
+export const sliderItems = [
+    {
+        id: 1,
+        img:"https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525085526vxpnm.png",
+        title: "SUMMER COLLECTION",
+        description: "GET 10% OFF FOR NEW ARRIVALS",
+        background:"lightblue",
+    },
+    {
+        id: 2,
+        img:"https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525085526vxpnm.png",
+        title: "WINTER CLEARANCE",
+        description: "GET UP TO 75% OFF FOR WINTER ITEMS",
+        background:"pink",
+    },
+    {
+        id: 3,
+        img:"https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525085526vxpnm.png",
+        title: "STUDENT WEEK",
+        description: "GET 15% OFF FOR ANY PURCHASE MORE THEN $100",
+        background:"lightgrey",
+    },
+]
+
+export const categories = [
+    {
+    id:1,
+    img:"https://images.pexels.com/photos/1721558/pexels-photo-1721558.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=1260&amp;h=750&amp;dpr=1",
+    title:"TOP"
+    },
+    {
+    id:2,
+    img:"https://images.pexels.com/photos/15312602/pexels-photo-15312602.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=1260&amp;h=750&amp;dpr=1",
+    title:"DRESSES"
+    },
+    {
+    id:3,
+    img:"https://images.pexels.com/photos/914668/pexels-photo-914668.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=1260&amp;h=750&amp;dpr=1",
+    title:"BOTTOM"
+    },
+]
+
+export const products = [
+    {
+    id:1,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+    {
+    id:2,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+    {
+    id:3,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+    {
+    id:4,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+    {
+    id:5,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+    {
+    id:6,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+    {
+    id:7,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+    {
+    id:8,
+    img:"https://cdn.mukama.com/31812-large_default/formal-friday-ultrafine-merino-t-shirt-black.jpg",
+    },
+]
+
+export const navItems = [
+    {
+        id:1,
+        category: 'Tops',
+        title: 'T-Shirts/Tank',
+    },
+    {
+        id:2,
+        category: 'Tops',
+        title: 'Shirts/Blouses',
+    },
+    {
+        id:3,
+        category: 'Tops',
+        title: 'Hoodies/Sweatshirts',
+    },
+    {
+        id:5,
+        category: 'Bottoms',
+        title: 'Jeans',
+    },
+    {
+        id:6,
+        category: 'Bottoms',
+        title: 'Shorts/Skirts',
+    },
+    {
+        id:7,
+        category: 'Bottoms',
+        title: 'Trousers',
+    },
+]
