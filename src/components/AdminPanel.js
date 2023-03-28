60ea637fb7e218ef1d9f21107bec9441e603ffa0
kayleephan <122582001+kayleephan@users.noreply.github.com>
2023-03-27 17:06:50 -0700

    fix conflict

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import AdminPanelRouter from "./AdminPanelRouter";
-<<<<<<< HEAD
 //import { BrowserRouter } from 'react-router-dom';
-=======
->>>>>>> fcd02c63f426c361b674c5d210b599f84f4435e4
 
 const AdminPanel = () => {
   return (
