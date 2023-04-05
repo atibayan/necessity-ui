7d383b507cc10da79e272b18c9771022de61f45d
requillom <requillom@student.douglascollege.ca>
2023-04-04 19:21:39 -0700

    Slider in product card and product view

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,114 @@
+// import React from "react";
+// import {
+//   Box,
+//   Card,
+//   CardActions,
+//   CardContent,
+//   CardMedia,
+//   Typography,
+//   Checkbox,
+// } from "@mui/material";
+// import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+// import Favorite from "@mui/icons-material/Favorite";
+// import { AddCartBtn, MinusCartBtn, QtyBtn, CartBtn } from "./CartButtons";
+// import { useShoppingCart } from "../context/ShoppingCartContext";
+// import { Link } from "react-router-dom";
+
+// const heartInactiveStyle = {
+//   color: "#a9a9a9",
+//   opacity: "80%",
+//   stroke: "white",
+//   strokeWidth: 2,
+// };
+
+// export default function ProductCard({ item }) {
+//   const { isInCart, addToWishlist, isInWishlist } = useShoppingCart();
+//   return (
+//     <Card
+//       sx={{
+//         maxWidth: 230,
+//         minWidth: 230,
+//         minHeight: "350px",
+//         position: "relative",
+//       }}>
+//       <Link to={`/product/${item._id}`}>
+//         <CardMedia
+//           sx={{ height: 230, width: 230, cursor: "pointer" }}
+//           image={item.images[1]?.signedImage}
+//           title={item.name}
+//         />
+//       </Link>
+
+//       <CardContent>
+//         <Typography
+//           gutterBottom
+//           variant="h6"
+//           component="div"
+//           sx={{
+//             minHeight: "70px",
+//           }}>
+//           {item.name.toUpperCase()}
+//         </Typography>
+//         <Box
+//           sx={{
+//             display: "flex",
+//             flexWrap: "nowrap",
+//             justifyContent: "space-between",
+//           }}>
+//           <Typography
+//             variant="h6"
+//             component="div"
+//             sx={{
+//               mt: 1,
+//             }}>
+//             CAD $ {(item.price * 1).toFixed(2)}
+//           </Typography>
+
+//           {isInCart(item._id) ? (
+//             <Box
+//               sx={{
+//                 display: "flex",
+//                 flexWrap: "nowrap",
+//                 justifyContent: "space-between",
+//               }}>
+//               <MinusCartBtn item={item} />
+//               <QtyBtn item={item} />
+//               <AddCartBtn item={item} />
+//             </Box>
+//           ) : (
+//             <CartBtn item={item} />
+//           )}
+//         </Box>
+//       </CardContent>
+//       <CardActions
+//         sx={{
+//           position: "absolute",
+//           display: "flex",
+//           flexDirection: "column-reverse",
+//           alignItems: "end",
+//           gap: "5px",
+//           top: 0,
+//           right: 0,
+//           margin: "5px",
+//           padding: 0,
+//         }}>
+//         <Box
+//           sx={{
+//             backgroundColor: "rgba(255, 255, 255, 0.2)",
+//             borderRadius: "50%",
+//           }}>
+//           <Checkbox
+//             icon={<FavoriteBorder />}
+//             checkedIcon={<Favorite />}
+//             onClick={() => addToWishlist(item._id)}
+//           />
+//         </Box>
+//       </CardActions>
+//     </Card>
+//   );
+// }
+
+
 import React from "react";
 import {
   Box,
@@ -13,6 +124,9 @@ import Favorite from "@mui/icons-material/Favorite";
 import { AddCartBtn, MinusCartBtn, QtyBtn, CartBtn } from "./CartButtons";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const heartInactiveStyle = {
   color: "#a9a9a9",
@@ -30,13 +144,27 @@ export default function ProductCard({ item }) {
         minWidth: 230,
         minHeight: "350px",
         position: "relative",
-      }}>
+      }}
+    >
       <Link to={`/product/${item._id}`}>
-        <CardMedia
-          sx={{ height: 230, width: 230, cursor: "pointer" }}
-          image={item.images[0]?.signedImage}
-          title={item.name}
-        />
+        <Slider
+          dots={true}
+          infinite={true}
+          speed={500}
+          slidesToShow={1}
+          slidesToScroll={1}
+          autoplay={true}
+          autoplaySpeed={3000}
+        >
+          {item.images.map((image) => (
+            <CardMedia
+              key={image._id}
+              sx={{ height: 230, width: 230, cursor: "pointer" }}
+              image={image.signedImage}
+              title={item.name}
+            />
+          ))}
+        </Slider>
       </Link>
 
       <CardContent>
--- src/pages/ProductScreen.jsx
+++ src/pages/ProductScreen.jsx
@@ -1,9 +1,110 @@
+// import { useShoppingCart } from "../context/ShoppingCartContext";
+// import { Box, Typography, Stack } from "@mui/material";
+// import styled from "styled-components";
+// import AddIcon from "@mui/icons-material/Add";
+// import RemoveIcon from "@mui/icons-material/Remove";
+// import { useParams } from "react-router-dom";
+
+// const ProductScreen = () => {
+//   const { products } = useShoppingCart();
+
+//   const routeParams = useParams();
+//   const selected = products.find((item) => item._id === routeParams.id);
+//   console.log(selected);
+//   return (
+//     <Box sx={{ width: "70%", mx: "auto", mt: "5vh" }}>
+//       {selected ? (
+//         <Stack
+//           gap={5}
+//           sx={{
+//             display: "flex",
+//             flexDirection: {
+//               xs: "column",
+//               md: "row",
+//             },
+//           }}>
+//           <Box sx={{ flexGrow: 1, width: 1 }}>
+//             <img
+//               src={selected.images[0]?.signedImage}
+//               alt={selected.images[0]?.signedImage}
+//               style={{
+//                 width: "100%",
+//                 aspectRatio: 1 / 1,
+//                 objectFit: "cover",
+//               }}
+//             />
+//           </Box>
+
+//           <Box sx={{ flexGrow: 1, width: 1 }}>
+//             <Typography variant="h3" p={1}>
+//               {selected.name}
+//             </Typography>
+//             <Typography p={1}>
+//               Product Description: {selected.description}
+//             </Typography>
+//             <Typography p={1}>Product ID: {selected._id}</Typography>
+//             <Typography variant="h6" p={1}>
+//               Price: CAD ${(selected.price * 1).toFixed(2)}
+//             </Typography>
+//             <AddContainer>
+//               <AmountContainer>
+//                 <RemoveIcon />
+//                 <Amount>1</Amount>
+//                 <AddIcon />
+//               </AmountContainer>
+//               <Button>ADD TO CART</Button>
+//             </AddContainer>
+//           </Box>
+//         </Stack>
+//       ) : null}
+//     </Box>
+//   );
+// };
+
+// const AddContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   width: 50%;
+//   justify-content: space-between;
+//   margin-top: 20px;
+// `;
+// const AmountContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+//   font-weight: 700;
+// `;
+
+// const Amount = styled.option`
+//   width: 30px;
+//   height: 30px;
+//   border-radius: 10px;
+//   border: 1px solid teal;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   margin: 0px 2px;
+// `;
+// const Button = styled.button`
+//   padding: 15px;
+//   border: 2px solid teal;
+//   background-color: white;
+//   pointer: cursor;
+//   font-weight: 500;
+// `;
+
+// export default ProductScreen;
+
+
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { Box, Typography, Stack } from "@mui/material";
 import styled from "styled-components";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import { useParams } from "react-router-dom";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProductScreen = () => {
   const { products } = useShoppingCart();
@@ -11,6 +112,18 @@ const ProductScreen = () => {
   const routeParams = useParams();
   const selected = products.find((item) => item._id === routeParams.id);
   console.log(selected);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  };
+  
+
   return (
     <Box sx={{ width: "70%", mx: "auto", mt: "5vh" }}>
       {selected ? (
@@ -22,17 +135,23 @@ const ProductScreen = () => {
               xs: "column",
               md: "row",
             },
-          }}>
-          <Box sx={{ flexGrow: 1, width: 1 }}>
-            <img
-              src={selected.images[0]?.signedImage}
-              alt={selected.images[0]?.signedImage}
-              style={{
-                width: "100%",
-                aspectRatio: 1 / 1,
-                objectFit: "cover",
-              }}
-            />
+          }}
+        >
+          <Box sx={{ flexGrow: 1, width: 0.4, padding: '30px'}}>
+            <Slider {...settings}>
+              {selected.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.signedImage}
+                  alt={image.signedImage}
+                  style={{
+                    width: "30% !important",
+                    aspectRatio: 1 / 1,
+                    objectFit: "cover",
+                  }}
+                />
+              ))}
+            </Slider>
           </Box>
 
           <Box sx={{ flexGrow: 1, width: 1 }}>
